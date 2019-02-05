8d344103b0b1afcbe97ecffcae9ffe8af79d8003
kasthurimahadeva <kasthuri.14@cse.mrt.ac.lk>
2019-02-05 12:19:18 +0530

    int-byte-short

--- src/com/java/Main.java
+++ src/com/java/Main.java
@@ -3,6 +3,17 @@ package com.java;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        int minIntNum = -2_147_483_648;
+        int maxIntNum = 2_147_483_647;
+        int intNum = (maxIntNum / 2);
+
+        byte minByteNum = -128;
+        byte maxByteNum = 127;
+        byte byteNum = (byte)(maxByteNum / 2);
+
+        short minShortNum = -32768;
+        short maxShortNum = 32767;
+        short shortNum = (short)(maxShortNum / 2);
     }
 }
